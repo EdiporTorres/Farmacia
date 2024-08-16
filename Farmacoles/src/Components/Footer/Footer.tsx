374aa2d0ff23f8eef099d1d0e0194a625ed6df93
Édipo Reis <ediporeiss@gmail.com>
2024-08-16 13:11:44 -0300

    SENHOR AMADO, quer dizer, Crud Produtos/Categorias/Login

--- Farmacoles/src/Components/Footer/Footer.tsx
+++ Farmacoles/src/Components/Footer/Footer.tsx
@@ -1,24 +1,20 @@
-import React from 'react'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
- 
-  
-
   return (
     <>
-        <div className="flex justify-center bg-indigo-900 text-white">
-          <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Farmacoles - Você em primeiro lugar| Copyright: </p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
-            </div>
+      <div className="flex justify-center bg-red-700 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className='text-xl font-bold'>Farmacoles - Você em primeiro lugar | Copyright: </p>
+          <p className='text-lg'>Acesse nossas redes sociais</p>
+          <div className='flex gap-2'>
+            <LinkedinLogo size={48} weight='bold' />
+            <InstagramLogo size={48} weight='bold' />
+            <FacebookLogo size={48} weight='bold' />
           </div>
         </div>
-      </>
+      </div>
+    </>
   )
 }
 
--- Farmacoles/src/Components/Navbar/NavBar.tsx
+++ Farmacoles/src/Components/Navbar/NavBar.tsx
@@ -1,29 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../Context/AuthContext'
+import { useContext } from 'react'
 
 function Navbar() {
- 
-  
+  let navigate = useNavigate()
 
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacoles- Voce em primeiro lugar</div>
+  const { usuario, handleLogout } = useContext(AuthContext)
 
-            <div className='flex gap-4'>
-            <Link to='/home' className='hover:underline'>Inicio</Link>
-            <Link to='/produto' className='hover:underline'>Produtos</Link>
-              <Link to='/categorias'className='hover:underline'>Categorias</Link>
-              <Link to='/cadastocategoria'className='hover:underline'>Cadastrar categoria de produto</Link>
-              <Link to='/perfil'  className='hover:underline'>Perfil</Link>
-              <div className='hover:underline'>Sair</div>
-            </div>
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
+
+  if(usuario.token !== "") {
+    navbarComponent = (
+   <div className='w-full bg-gradient-to-r from-gray-800  to-slate-500  text-white flex justify-center py-4'>
+    
+      <div className='w-full bg-red-700 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>Farmacoles - Você em primeiro lugar</div>
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Início</Link>
+            <Link to='/produtos' className='hover:underline'>Produtos</Link>
+            <Link to='/Categorias'className='hover:underline'>Categorias</Link>
+            <Link to='/cadastrocategoria'className='hover:underline'>Cadastrar categoria de produto</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
-    </>
+      </div>
+      </div>
+  
   )
+}
+return (
+<>
+  {navbarComponent}
+</>
+)
 }
 
 export default Navbar
